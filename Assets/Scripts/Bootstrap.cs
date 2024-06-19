5b43e2a90f24a83ab7e41a607764bbf23cdb2c02
gitGutBro <103259112+gitGutBro@users.noreply.github.com>
2024-06-19 15:32:42 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bootstrap : MonoBehaviour
+{
+    [Header("Image Settings")]
+    [SerializeField][Range(0.01f, 1f)] private float _changingStep;
+    [SerializeField][Range(0f, 1f)] private float _targetAlpha;
+    [SerializeField] private int _durationStepInMilliseconds;
+    [SerializeField] private Image _image;
+    [Space]
+    [Header("Transform Settings")]
+    [SerializeField][Range(0.01f, 1f)] private float _movingStep;
+    [SerializeField] private Vector3 _targetPosition;
+    [SerializeField] private GameObject _objectToMove;
+
+    private async void Start()
+    {
+        _image.ChangeAlphaAsync(_targetAlpha, _changingStep, _durationStepInMilliseconds);
+        _objectToMove.transform.MoveAsync(_targetPosition, _movingStep);
+    }
+}
---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class ExtensionMethods
+{
+    public static async UniTask ChangeAlphaAsync(this Image image, float targetAlpha, float changingStep, int durationStepInMilliseconds)
+    {
+        while (Mathf.Abs(image.color.a - targetAlpha) > 0.001f)
+        {
+            float alpha = Mathf.MoveTowards(image.color.a, targetAlpha, changingStep);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+            await UniTask.Delay(durationStepInMilliseconds);
+        }
+    }
+
+    public static async UniTask MoveAsync(this Transform transform, Vector3 targetPosition, float movingStep)
+    {
+        while (Vector3.Distance(transform.position, targetPosition) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movingStep);
+            await UniTask.Yield();
+        }
+    }
+}
